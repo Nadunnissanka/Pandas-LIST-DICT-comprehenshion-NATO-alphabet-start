9c46881d8e6ada6b3721734d743673c775db6317
Nadun Nissanka <61065783+Nadunnissanka@users.noreply.github.com>
2021-07-28 00:06:07 +0530

    error handling added

--- main.py
+++ main.py
@@ -1,32 +1,23 @@
 import pandas
 
-student_dict = {
-    "student": ["Angela", "James", "Lily"],
-    "score": [56, 76, 98]
-}
-
-# Looping through dictionaries:
-for (key, value) in student_dict.items():
-    # Access key and value
-    pass
-
-student_data_frame = pandas.DataFrame(student_dict)
-
-# Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    # Access index and row
-    # Access row.student or row.score
-    pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
 # TODO 1. Create a dictionary in this format:
 dataframe = pandas.read_csv("nato_phonetic_alphabet.csv")
 data_dict = {row.letter: row.code for (index, row) in dataframe.iterrows()}
 
+
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word that you wish to convert in to Nato format: ").upper()
-word_char_list = list(word)
-nato_alpha = [data_dict[letter] for letter in word_char_list]
-print(nato_alpha)
+def nato_converter():
+    word = input("Enter a word that you wish to convert in to Nato format: ").upper()
+    word_char_list = list(word)
+    nato_alpha = [data_dict[letter] for letter in word_char_list]
+    print(nato_alpha)
+
+
+loop = True
+
+while loop:
+    try:
+        nato_converter()
+        loop = False
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
